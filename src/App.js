393ac2df36f7837f31075f5a3b8bd514390c3f26
haisheng2017 <895883668@qq.com>
2019-07-11 19:25:37 +0800

    react virtul node : fragment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
 import Main from './components/MainComponent';
 class App extends Component {
   
